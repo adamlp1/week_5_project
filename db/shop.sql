d9a6221f962045577c249bc04c29e107edba472f
adam <adam.lewispetrie@gmail.com>
2020-03-14 21:31:37 +0000

    updates db table

--- db/shop.sql
+++ db/shop.sql
@@ -1,6 +1,7 @@
 DROP TABLE products;
 
 CREATE TABLE products (
+  id SERIAL4,
   name VARCHAR(255),
   description VARCHAR(255),
   stock INT,
--- models/product.rb
+++ models/product.rb
@@ -3,8 +3,10 @@ require_relative('../db/sql_runner')
 class Product
 
   attr_accessor :name, :description, :stock, :buy_cost, :sell_price
+  attr_reader :id
 
   def initialize(options)
+    @id = options['id'].to_i if options['id']
     @name = options['name']
     @description = options['description']
     @stock = options['stock'].to_i
@@ -24,9 +26,15 @@ class Product
     VALUES
     (
       $1, $2, $3, $4, $5
-    )"
+    )
+    RETURNING id"
     values = [@name, @description, @stock, @buy_cost, @sell_price]
     result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id
+  end
+
+  def update()
   end
 
 
