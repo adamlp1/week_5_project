1fdc49e3c8931f3b9918fac349845649f9f066b5
adam <adam.lewispetrie@gmail.com>
2020-03-15 14:00:57 +0000

    adds manu methods

---
+++ models/manufacturer.rb
@@ -0,0 +1,63 @@
+require_relative('sql_runner.rb')
+
+class Manufacturer
+
+attr_reader :name, :id
+
+def initialize(options)
+  @name = options['name']
+  @id = options['id'].to_i if options['id']
+end
+
+def save()
+  sql = "INSERT INTO manufacturers
+  (
+    name
+  )
+  VALUES
+  (
+    £1
+  )
+  RETURNING id"
+  values = [@name]
+  result = SqlRunner.run(sql, values)
+  id = result.first["id"]
+  @id = id.to_i
+end
+
+def self.delete_all()
+  sql = "DELETE FROM manufacturers;"
+  SqlRunner.run(sql)
+end
+
+def self.all()
+  sql = "SELECT * FROM manufacturers"
+  manufacturers = SqlRunner.run(sql)
+  reuslt = manufacturers.map { |manufacturer| Manufacturer.new(manufacturer) }
+  return result
+end
+
+def self.find(id)
+  sql = "SELECT * FROM manufacturers WHERE id = $1"
+  values = [id]
+  manufacturer = SqlRunner.run(sql, values)
+  result = Manufacturer.new(manufacturer.first)
+  return result
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
